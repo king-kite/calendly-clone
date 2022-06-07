b44f92b45e639852e1c912c2887a6d7ad05e94a2
kazeem <holayinka5050@gmail.com>
2022-06-07 14:08:26 +0100

    Changed the public directory in the vite.config.js file from src/public to public

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  publicDir: "src/public",
+  publicDir: "public",
   root: "src"
 })
